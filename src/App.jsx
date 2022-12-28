855a5ba72115f061d835971b48f39ddaebba79fb
summertree128 <natsukinoue530000@gmail.com>
2022-12-29 04:08:56 +0900

    npm run fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import useSWR from 'swr'
+import useSWR from "swr";
 
 function App() {
-  const url = "https://httpstat.us/200?sleep=2000";
+  const url = "https://aaaaa.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
 
   const fetcher = (url) => fetch(url, { headers }).then((res) => res.json());
-  const { data, error, isLoading } = useSWR(url, fetcher)
+  const { data, error, isLoading } = useSWR(url, fetcher);
 
-  if (error) return <p>Failed to load.</p>
-  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Failed to load.</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   const status = data.description;
 
